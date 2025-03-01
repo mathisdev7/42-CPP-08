b33841a3d4dffbf0a6dfe411c0f5056e6c6afd96
Frost <999frostz@gmail.com>
2025-03-01 18:56:02 +0100

    fix(ex02): fix member functions already implemented in std::stack, add only iterators

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -16,82 +16,26 @@
 #include <exception>
 
 template <typename T>
-class MutantStack
+class MutantStack : public std::stack<T>
 {
-private:
-	std::stack<T> stack;
 
 public:
 	MutantStack() {}
-	MutantStack(const MutantStack &other) : stack(other.stack) {}
+	MutantStack(const MutantStack &other) : std::stack<T>(other) {}
 	MutantStack &operator=(const MutantStack &other)
 	{
 		if (this != &other)
 		{
-			stack = other.stack;
+			std::stack<T>::operator=(other);
 		}
 		return *this;
 	}
 
 	~MutantStack() {}
 
-	void push(const T &value) { stack.push(value); }
-	void pop()
-	{
-		if (!stack.empty())
-			stack.pop();
-	}
-	T &top()
-	{
-		if (!stack.empty())
-			return stack.top();
-	}
-	const T &top() const
-	{
-		if (!stack.empty())
-			return stack.top();
-	}
-	bool empty() const { return stack.empty(); }
-	size_t size() const { return stack.size(); }
+	typedef typename std::stack<T>::container_type::iterator iterator;
 
-	class iterator
-	{
-	private:
-		std::deque<T> elements;
-		typename std::deque<T>::iterator current;
+	iterator begin() { return this->c.begin(); }
 
-	public:
-		iterator(std::stack<T> &stack)
-		{
-			std::stack<T> temp = stack;
-			while (!temp.empty())
-			{
-				elements.push_front(temp.top());
-				temp.pop();
-			}
-			current = elements.begin();
-		}
-
-		T &operator*() { return *current; }
-		T *operator->() { return &(*current); }
-
-		iterator &operator++()
-		{
-			++current;
-			return *this;
-		}
-		iterator operator++(int)
-		{
-			iterator tmp = *this;
-			++(*this);
-			return tmp;
-		}
-
-		bool operator!=(const iterator &other) const { return current != other.current; }
-		bool operator==(const iterator &other) const { return current == other.current; }
-	};
-
-	iterator begin() { return iterator(stack); }
-
-	iterator end() { return iterator(stack); }
+	iterator end() { return this->c.end(); }
 };
